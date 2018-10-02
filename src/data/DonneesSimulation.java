0be95a43d5e4cdc62a68636329894b5a244d9ae7
Alexis Bigé <Alexis.Big@grenoble-inp.org>
2018-10-02 15:08:00 +0200

    Classe données simulation

--- src/data/DonneesSimulation.java
+++ src/data/DonneesSimulation.java
@@ -11,8 +11,27 @@ public class DonneesSimulation {
     /**
      * Classe DonneesSimulation : classe principale regroupant toutes les données du problème
      */
+	
+	/* Attributs */
+	private Carte carte;
+	private Incendie[] incendies;
+	private Robot[] robots;
 
+	/* Constructeur */
 	public DonneesSimulation(Carte carte, Incendie[] incendies, Robot[] robots) {
-		// TODO
+		this.carte = carte;
+		this.incendies = incendies;
+		this.robots = robots;
+	}
+	
+	/* Accesseurs */
+	public Carte getCarte() {
+		return this.carte;
+	}
+	public Incendie[] getIncendies() {
+		return this.incendies;
+	}
+	public Robot[] getRobot() {
+		return this.robots;
 	}
 }
